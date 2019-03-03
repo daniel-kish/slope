46f2bd578e5a649c37bdc481c384b91d48d9ced6
Daniel <daniel_kish@outlook.com>
2019-03-03 23:10:30 +0300

    No correction and a Wol nb.

--- code.py
+++ code.py
@@ -1,5 +1,14 @@
 import sys
 
+
+# print for wolfram
+def wol_print(lst):
+	fmt_str = []
+	for el in lst:
+		fmt_str.append('{{{:03.4f}, {:03.4f}}}'.format(*el))
+	print('{' + ', '.join(fmt_str) + '}')
+
+
 pts = list()
 with open(sys.argv[1]) as f:
 	for line in f.readlines():
@@ -7,9 +16,6 @@ with open(sys.argv[1]) as f:
 		pts.append([float(x) for x in line.split(' ')])
 
 pts.sort(key=lambda x: x[1])
-
-for x in pts:
-	print('{{{:03.2f}, {:03.2f}}}'.format(*x))
 
 velocities = []
 
@@ -21,28 +27,17 @@ def fin_dif(p1, p2):
 for i in range(0, len(pts)-1):
 	velocities.append(fin_dif(pts[i], pts[i+1]))
 
-print('\n')
-for x in velocities:
-	print('{{{:03.2f}, {:03.2f}}}'.format(*x))
-
 accelerations = []
 for i in range(0, len(velocities)-1):
 	accelerations.append(fin_dif(velocities[i], velocities[i+1]))
 
-print('\n')
-for x in accelerations:
-	print('{{{:03.2f}, {:03.2f}}}'.format(*x))
-
-print('\n')
-
-# print for wolfram
-def wol_print(lst):
-	fmt_str = []
-	for el in lst:
-		fmt_str.append('{{{:03.4f}, {:03.4f}}}'.format(*el))
-	print('{' + ', '.join(fmt_str) + '}')
-
+print('position:')
 wol_print(pts)
+print('velocity:')
+wol_print(velocities)
+print('acceleration:')
+wol_print(accelerations)
+print('\n')
 
 a_av = 0.0
 for x in accelerations: a_av += x[1]
@@ -58,10 +53,3 @@ print('a_ov = {}'.format(2.0 * s_full/(t_full*t_full)))
 from math import cos, sin, pi
 
 print('\nprediction:\na = {}'.format(9.8*sin(45.0*pi/180.0) - 9.8*0.35*cos(45.0*pi/180.0)))
-
-def correct(l, L, p, y):
-	return y - (l/L)*(p-y)
-
-for p in pts:
-	print('{:03.4f}, {:03.4f}'.format(p[0],
-		  correct(0.005, 0.2, 0.210, p[1])))
