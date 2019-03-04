e0d325aaf7893f67ee1514599a6cff10bdfe7ae7
Daniel Kishlakov <Daniel.Kishlakov@acronis.com>
2019-03-04 10:17:20 +0300

    Add a new data set.

--- code.py
+++ code.py
@@ -12,37 +12,37 @@ def wol_print(lst):
 pts = list()
 with open(sys.argv[1]) as f:
 	for line in f.readlines():
-		# print(line)
-		pts.append([float(x) for x in line.split(' ')])
+		parts = line.split(' ')
+		pts.append([float(parts[0]), float(parts[1])])
 
 pts.sort(key=lambda x: x[1])
-
-velocities = []
 
 def fin_dif(p1, p2):
 	dfdt = (p2[1] - p1[1])/(p2[0] - p1[0])
 	t = (p2[0] + p1[0]) / 2
 	return [t, dfdt]
 
+v = []
 for i in range(0, len(pts)-1):
-	velocities.append(fin_dif(pts[i], pts[i+1]))
+	v.append(fin_dif(pts[i], pts[i+1]))
 
-accelerations = []
-for i in range(0, len(velocities)-1):
-	accelerations.append(fin_dif(velocities[i], velocities[i+1]))
+a = []
+for i in range(0, len(v)-1):
+	a.append(fin_dif(v[i], v[i+1]))
 
-print('position:')
-wol_print(pts)
-print('velocity:')
-wol_print(velocities)
-print('acceleration:')
-wol_print(accelerations)
-print('\n')
+if len(sys.argv) > 2 and sys.argv[2] == '-v':
+	print('position:')
+	wol_print(pts)
+	print('velocity:')
+	wol_print(v)
+	print('acceleration:')
+	wol_print(a)
+	print('\n')
 
 a_av = 0.0
-for x in accelerations: a_av += x[1]
+for x in a: a_av += x[1]
 
-a_av /= len(accelerations)
+a_av /= len(a)
 
 print('<a> = {}'.format(a_av))
 
@@ -50,6 +50,6 @@ t_full = pts[-1][0] - pts[0][0]
 s_full = pts[-1][1] - pts[0][1]
 print('a_ov = {}'.format(2.0 * s_full/(t_full*t_full)))
 
-from math import cos, sin, pi
+# from math import cos, sin, pi
 
-print('\nprediction:\na = {}'.format(9.8*sin(45.0*pi/180.0) - 9.8*0.35*cos(45.0*pi/180.0)))
+# print('\nprediction:\na = {}'.format(9.8*sin(45.0*pi/180.0) - 9.8*0.35*cos(45.0*pi/180.0)))
