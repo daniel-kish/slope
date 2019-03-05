ef633a16217807b986b30df4d15ff837bd351ec1
Daniel Kishlakov <Daniel.Kishlakov@acronis.com>
2019-03-05 13:55:28 +0300

    Add data set #6.

--- conv.py
+++ conv.py
@@ -1,14 +1,21 @@
 def part_sum(v, i):
 	return sum(v[0:i])
 
-vals = [5.344, 4.09517309249, 3.91929162433, 4.016, 5.33324867859]
-# vals = [5.95991634919, 4.43063482819, 4.30648319934, 4.52429456631, 4.4]
+vals_av = [5.95991634919, 4.43063482819, 4.30648319934, 4.52429456631, 4.4, 5.2]
+vals_ov = [5.344, 4.09517309249, 3.91929162433, 4.016, 3.616, 4.256]
 
-part_sums = []
-for i in range(1, len(vals)+1):
-	part_sums.append(part_sum(vals, i) / i)
+def fun(vals):
+	part_sums = []
+	for i in range(1, len(vals)+1):
+		part_sums.append(part_sum(vals, i) / i)
 
-for i in range(0, len(part_sums)-1):
-	print(abs(part_sums[i+1] - part_sums[i]))
+	print('convergence')
+	for i in range(0, len(part_sums)-1):
+		print(abs(part_sums[i+1] - part_sums[i]))
 
-print('<a> = {}'.format(part_sums[-1]))
+	print('mean value = {}\n'.format(part_sums[-1]))
+
+print('average values:')
+fun(vals_av)
+print('overall values:')
+fun(vals_ov)
