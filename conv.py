c446aec0715d7ea922f4bda51073d880c6c58fd7
Daniel Kishlakov <Daniel.Kishlakov@acronis.com>
2019-03-05 11:11:46 +0300

    Add data set #5.

--- conv.py
+++ conv.py
@@ -1,7 +1,8 @@
 def part_sum(v, i):
 	return sum(v[0:i])
 
-vals = [5.344, 4.09517309249, 3.91929162433, 4.016]
+vals = [5.344, 4.09517309249, 3.91929162433, 4.016, 5.33324867859]
+# vals = [5.95991634919, 4.43063482819, 4.30648319934, 4.52429456631, 4.4]
 
 part_sums = []
 for i in range(1, len(vals)+1):
@@ -9,3 +10,5 @@ for i in range(1, len(vals)+1):
 
 for i in range(0, len(part_sums)-1):
 	print(abs(part_sums[i+1] - part_sums[i]))
+
+print('<a> = {}'.format(part_sums[-1]))
